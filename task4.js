486a2fcc21d54c8a34218505e3e0d2c4ddada568
Gayane25 <gayabennu@gmail.com>
2021-12-01 20:39:37 +0400

    swap two variables without third

---
+++ task4.js
@@ -0,0 +1,7 @@
+//Swap two variables without using the third one
+
+let a = 10;
+let b = 5;
+[a, b] = [b, a];
+console.log(a);
+console.log(b); // swapping without third variable,  searched in google this kind of swapping.
