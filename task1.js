0ddb3e43d26b3df6754753b7fd4d8c4912d6a533
Gayane25 <gayabennu@gmail.com>
2021-12-01 20:18:25 +0400

    task about variables string value

---
+++ task1.js
@@ -0,0 +1,10 @@
+/*
+. Declare two variables: admin and name.
+. Write the string "John" to the name variable.
+. Copy the value from the name variable to admin.
+. Display the value admin. */
+
+let admin, name1;
+name1 = 'John';
+admin = name1;
+console.log(admin);
